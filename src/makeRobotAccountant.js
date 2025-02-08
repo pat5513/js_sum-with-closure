1a6cdc108e24ad8c71930413160a7da7a9f07f17
Patryk Czarnocki <pat5513@gmail.com>
2025-02-08 18:15:02 +0100

    solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,21 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let counter = 0;
+
+  return function(num1) {
+    return function(num2) {
+      counter++;
+
+      if (counter <= 3) {
+        return num1 + num2;
+      } else if (counter % 2 === 0) {
+        return 'Bzzz... Error!';
+      } else {
+        return num1 + num2;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
